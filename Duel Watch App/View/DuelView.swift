739578b541fe6a33e2b40c7fc49141179345792d
Jacob Kerames <mail@jacobkerames.com>
2023-07-09 14:33:03 -0400

    Separated the game logic and UI for organization

--- Duel Watch App/View/DuelView.swift
+++ Duel Watch App/View/DuelView.swift
@@ -5,20 +5,79 @@
 //  Created by Jacob Kerames on 10/19/22.
 //
 
-import SwiftUI
+import Foundation
+import WatchConnectivity
+import Combine
 
-struct DuelView: View {
-    var body: some View {
-        
-        //If user has entered username and dominant hand, begin duel
-        //If not, have them enter the information
-        //Continue to duel
-        Text("Hello World")
+class DuelView: ObservableObject {
+    @Published var duelResult: String = ""
+    @Published var countdown = 3
+    @Published var isCountingDown = false
+    @Published var didDrawEarly = false
+    @Published var isDuelStarted = false
+    @Published var isDrawDetected = false
+
+    let countdownTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    private let wcDelegate = ExtensionDelegate.shared
+    private let motionManager = MotionManager()
+    
+    init() {
+        _ = wcDelegate.$message.compactMap { $0 }
+            .sink(receiveValue: { message in
+                if let action = message["action"] as? String {
+                    let timestamp = Date().timeIntervalSince1970
+                    self.wcDelegate.sendMessage(["action": "draw", "timestamp": timestamp])
+
+                    switch action {
+                    case "countdownStart":
+                        self.isDuelStarted = true
+                        self.isCountingDown = true
+                        self.motionManager.startUpdates()
+                    case "duelResult":
+                        if let result = message["result"] as? String {
+                            self.duelResult = result
+                            self.motionManager.stopUpdates()
+                        }
+                    default:
+                        break
+                    }
+
+                    let theirTimestamp = message["timestamp"] as? TimeInterval
+                    let ourTimestamp = Date().timeIntervalSince1970
+                    let averageTimestamp = ((theirTimestamp ?? 0) + ourTimestamp) / 2
+
+                    if theirTimestamp! < averageTimestamp {
+                        // They drew first
+                    } else {
+                        // We drew first
+                    }
+                }
+            })
     }
-}
+    
+    func startDuel() {
+        wcDelegate.sendMessage(["action": "start"])
+    }
 
-struct DuelView_Previews: PreviewProvider {
-    static var previews: some View {
-        DuelView()
+    func joinDuel() {
+        wcDelegate.sendMessage(["action": "connect"])
+    }
+
+    func countdownTick() {
+        if countdown > 0 {
+            countdown -= 1
+        } else {
+            isCountingDown = false
+        }
+    }
+
+    func drawDetected() {
+        if isCountingDown {
+            didDrawEarly = true
+            wcDelegate.sendMessage(["action": "earlyDraw"])
+        } else if countdown == 0 && !didDrawEarly {
+            let timestamp = Date().timeIntervalSince1970
+            wcDelegate.sendMessage(["action": "draw", "timestamp": timestamp])
+        }
     }
 }
--- Duel Watch App/View/InitialView.swift
+++ Duel Watch App/View/InitialView.swift
@@ -6,28 +6,16 @@
 //
 
 import SwiftUI
-import WatchConnectivity
 
-// Main user interface for the Watch app
 struct InitialView: View {
-    @StateObject private var wcDelegate = ExtensionDelegate.shared
-    @StateObject private var motionManager = MotionManager()
-
-    @State private var duelResult: String = ""
-    @State private var countdown = 3
-    @State private var isCountingDown = false
-    @State private var didDrawEarly = false
-    @State private var isDuelStarted = false
-    @State private var isDrawDetected = false
-
-    let countdownTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @StateObject private var viewModel = DuelView()
 
     var body: some View {
         VStack {
             // Only show the buttons if the duel hasn't started yet.
-            if !isDuelStarted {
+            if !viewModel.isDuelStarted {
                 Button(action: {
-                    wcDelegate.sendMessage(["action": "start"])
+                    viewModel.startDuel()
                 }) {
                     HStack {
                         Image(systemName: "hand.point.right.fill")
@@ -36,7 +24,7 @@ struct InitialView: View {
                 }
 
                 Button(action: {
-                    wcDelegate.sendMessage(["action": "connect"])
+                    viewModel.joinDuel()
                 }) {
                     HStack {
                         Image(systemName: "arrow.right.circle.fill")
@@ -45,60 +33,20 @@ struct InitialView: View {
                 }
             }
 
-            if isCountingDown {
-                Text("Countdown: \(countdown)")
-                    .onReceive(countdownTimer) { _ in
-                        if countdown > 0 {
-                            countdown -= 1
-                        } else {
-                            isCountingDown = false
-                        }
-                    }
-            } else if countdown == 0 && isDuelStarted {
+            if viewModel.isCountingDown {
+                Text("Countdown: \(viewModel.countdown)")
+            } else if viewModel.countdown == 0 && viewModel.isDuelStarted {
                 Text("DRAW!")
             } else {
-                Text(duelResult)
+                Text(viewModel.duelResult)
             }
         }
-        .onReceive(wcDelegate.$message.compactMap { $0 }) { message in
-            if let action = message["action"] as? String {
-                let timestamp = Date().timeIntervalSince1970
-                wcDelegate.sendMessage(["action": "draw", "timestamp": timestamp])
-
-                switch action {
-                case "countdownStart":
-                    isDuelStarted = true
-                    isCountingDown = true
-                    motionManager.startUpdates()
-                case "duelResult":
-                    if let result = message["result"] as? String {
-                        duelResult = result
-                        motionManager.stopUpdates()
-                    }
-                default:
-                    break
-                }
-
-                let theirTimestamp = message["timestamp"] as? TimeInterval
-                let ourTimestamp = Date().timeIntervalSince1970
-                let averageTimestamp = ((theirTimestamp ?? 0) + ourTimestamp) / 2
-
-                if theirTimestamp! < averageTimestamp {
-                    // They drew first
-                } else {
-                    // We drew first
-                }
-            }
+        .onReceive(viewModel.countdownTimer) { _ in
+            viewModel.countdownTick()
         }
-        .onChange(of: isDrawDetected) { drawDetected in
+        .onChange(of: viewModel.isDrawDetected) { drawDetected in
             if drawDetected {
-                if isCountingDown {
-                    didDrawEarly = true
-                    wcDelegate.sendMessage(["action": "earlyDraw"])
-                } else if countdown == 0 && !didDrawEarly {
-                    let timestamp = Date().timeIntervalSince1970
-                    wcDelegate.sendMessage(["action": "draw", "timestamp": timestamp])
-                }
+                viewModel.drawDetected()
             }
         }
     }
